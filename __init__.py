1702a98220fc85e8b702c8dd44702958174d6cf4
Immanuel Albrecht <immanuel.albrecht@dlh.de>
2022-07-09 10:51:41 +0200

    added routine that gives possible out-neighbors for routing augmentations

--- __init__.py
+++ __init__.py
@@ -43,6 +43,62 @@ class TransversalMatroid(sage.matroids.matroid.Matroid):
         return len(GX.matching())
         
         
-    def __repr__(self):
+    def _repr_(self):
         return f"TransversalMatroid({self.facemap})"
+       
+def outboundNeighbors(D, paths, visited=None, V=None):
+    """ computes the outbound neighbors that are accessible from each vertex wrt. to a given (partial) routing
+        D       __ sage.graphs.digraph.DiGraph object
+        paths   __ set/list consisting of the paths of the routing (as vertex-sequence)
+        visited __ (optional) set of vertices visited by the paths
+        V       __ (optional) set of vertices in D
         
+        returns a dictionary that maps each vertex to a set of triples
+            (v,X,(u,v))
+        where X consists of edges of a visited path that need to be removed from the routing
+           and (u,v) is the edge that needs to be added to the routing 
+           in order to use the connection
+    """
+    if visited is None:
+        visited = frozenset().union(*paths)
+    if V is None:
+        V = frozenset(D.vertex_iterator())
+    D = sage.graphs.digraph.DiGraph(D)
+        
+    out = dict()
+    
+    for nonvisited in V.difference(visited):
+        out[nonvisited] = frozenset([(v,frozenset(),(u,v)) for u,v,lbl in D.outgoing_edges(nonvisited) if v in V])
+        
+    for p in paths:
+        Vp = frozenset(p) # vertices reached by the path
+        neighbors = {}
+        # initialize neighbors
+        for u,v,_ in D.outgoing_edges(p[0]):
+            if v in Vp:
+                continue # do not jump back on the path
+            neighbors[v] = (frozenset(),(u,v))
+        
+        for u,v in zip(p[:-1],p[1:]):
+            #augment the list of dropped edges
+            neighbors = dict((
+                (t,(X.union({(u,v)}),e))            
+                for (t,(X,e)) in neighbors.items()
+            ))
+            
+            # by altering the path that visits v (and permuting targets in the routing),
+            # we may go from v to any of the previous outbound neighbors on the path that
+            # are not visited by this path when searching for an augmentation of the routing
+            out[v] = frozenset((t,X,e) for t,(X,e) in neighbors.items())
+            
+            #update the neighbors: add new possible targets for subsequent vertices in the path
+            for _,w,_ in D.outgoing_edges(v):
+                if w in Vp:
+                    continue # no back-jumps!
+                if w in neighbors:
+                    continue # we already have a way to get to w, which frees more vertices
+                neighbors[w] = (frozenset(),(v,w))
+    for v in visited:
+        if not v in out:
+            out[v] = frozenset()
+    return out
